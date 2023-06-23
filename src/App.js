2853f8bb7ed582f7c8aac04dc13e71408ad882f5
Philippe Miossec <pmiossec@gmail.com>
2023-06-23 15:15:31 +0200

    Iteration 2: ApartmentsList

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import ironhackersImg from "./assets/ironhackers.avif"
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
@@ -6,15 +5,32 @@ import ApartmentsList from "./components/ApartmentsList";
 import HomePage from "./components/HomePage";
 import ApartmentDetails from "./components/ApartmentDetails";
 import CreateApartment from "./components/CreateApartment";
-
+import { useState,useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [apartments, setApartments] = useState(null);
+
+  useEffect(() => {
+		getApartmentsFromApi();
+	}, []);
+
+
+  const getApartmentsFromApi = () =>{
+    axios
+      .get(`https://ironbnb-m3.herokuapp.com/apartments`)
+      .then((response)=>{
+        setApartments(response.data)
+      })
+      .catch((e)=>{console.log(e)})
+  }
+
   return (
     <div className="App">
       <Navbar />
         <Routes>
           <Route path='/' element={<HomePage />}/>
-          <Route path='/apartments' element={<ApartmentsList />} />
+          <Route path='/apartments' element={<ApartmentsList apartments={apartments}/>} />
           <Route path='/apartments/:id' element={<ApartmentDetails />} />
           <Route path='/apartments/create' element={<CreateApartment />} />
         </Routes>
--- src/components/ApartmentsList.js
+++ src/components/ApartmentsList.js
@@ -1,4 +1,27 @@
-export default function ApartmentsList() {
-    return (
-        <h1>Apartments list</h1> )
+
+import { Link } from "react-router-dom";
+
+export default function ApartmentsList({apartments}) {
+    
+         if(apartments=== null){
+                return <p>loading...</p>
+            }   
+        console.log(apartments);
+    return (<div>
+
+        {apartments.map((apartment)=>{
+
+            return(
+                <div key={apartment._id} className='apartment-box'>
+                    <p>Title: {apartment.title}</p>
+                    <p>Price per day: {apartment.pricePerDay}</p>
+                    <img src={apartment.img} alt={apartment.title} />
+                    <Link to={`/apartments/${apartment._id}`}>More Details</Link>
+                </div>
+            )
+       
+        })}
+        
+
+    </div>)
 }

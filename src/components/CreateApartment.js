689cffdfb4ec232a58d7fbeca35712a11bca7366
Philippe Miossec <pmiossec@gmail.com>
2023-06-23 15:23:28 +0200

    Iteration 3: Create new Apartment

--- src/components/CreateApartment.js
+++ src/components/CreateApartment.js
@@ -1,4 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function CreateApartment() {
+
+    const [title, setTitle] = useState("");
+    const [img, setImg] = useState("");
+    const [pricePerDay, setPricePerDay] = useState("");
+
+    const navigate = useNavigate();
+
+
+    const createNewApartment = e => {
+        e.preventDefault();
+        axios.post(`https://ironbnb-m3.herokuapp.com/apartments`, {
+            title, img, pricePerDay
+        })
+        .then(r => {
+            console.log("created", r.data);
+            navigate("/apartments");
+        })
+        .catch(console.log);
+    }
+
     return (
-        <h1>Create Apartment</h1> )
+        <div id="create-appartment">
+        <h1>Create Apartment</h1>
+        <form onSubmit={e => createNewApartment(e)}>
+
+            <label>Title:
+                <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            </label>
+            <label>Image Url:
+                <input type="text" value={img} onChange={e => setImg(e.target.value)} />
+            </label>
+            <label>€/day
+                <input type="number" value={pricePerDay} onChange={e => setPricePerDay(e.target.value)} />
+            </label>
+
+            <button type="submit">Create</button>
+
+        </form>
+        </div>);
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom"
 
 function NavBar(){
-    return(<div className="NavBar">
+    return(<div className="navbar">
     <NavLink to="/">Home</NavLink>
     <NavLink to="/apartments">Apartments</NavLink>
     <NavLink to="/apartments/create">Create</NavLink>
